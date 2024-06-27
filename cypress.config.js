992d7de7c367be847c866df32a2402fd2621e61e
Javier Picon <javierpicon@Javiers-MacBook-Pro.local>
2024-06-27 16:42:58 -0400

    change tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'yb5e2a',
-  
   e2e: {
     baseUrl: 'https://portal.dev.digitalu.teambespin.us/app/support',
-
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure your specPattern is set correctly
+    setupNodeEvents(on, config) {
+      // Implement node event listeners here
+    },
+    projectId: 'yb5e2a',
+    record: true,
+    key: process.env.CYPRESS_RECORD_KEY || 'cd82295e-748c-405d-9e9b-532be05b2e2a',
   },
-  node_modules: "/opt/homebrew/lib/node_modules",
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 60000,
   viewportWidth: 1280, // Set your desired width here
   viewportHeight: 720  // Set your desired height here
-  
 });
